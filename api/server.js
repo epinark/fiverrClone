f4b41f2395d51493733e0535218b07d7941a3818
epinark <122348695+epinark@users.noreply.github.com>
2024-01-09 16:51:09 +0100

    basic structure

---
+++ api/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+const app = express();
+dotenv.config();
+mongoose.set("strictQuery", true);
+
+
+const connect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO);
+        console.log("Connected to mongoDB!");
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+app.listen(8800, () => {
+    connect();
+    console.log("BAckend server is running!");
+})

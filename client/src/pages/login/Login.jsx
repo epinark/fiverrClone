c5c14b0ac35b54f2c0387c1e063d76f3f6055579
epinark <122348695+epinark@users.noreply.github.com>
2024-01-10 12:18:50 +0100

    cloudinary addition

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,8 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.scss";
+import newRequest from "../../utils/newRequest";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
-  return <div>Login</div>;
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await newRequest.post("/auth/login", {
+        username,
+        password,
+      });
+      localStorage.setItem("currentUser", JSON.stringify(res.data));
+      navigate("/");
+    } catch (err) {
+      setError(err.response.data);
+    }
+  };
+
+  return (
+    <div className="login">
+      <form onSubmit={handleSubmit}>
+        <h1>Sign in</h1>
+        <label htmlFor="">Username</label>
+        <input
+          type="text"
+          name="username"
+          placeholder="johndoe"
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <label htmlFor="">Password</label>
+        <input
+          type="password"
+          name="password"
+          placeholder="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button className="submit">Login</button>
+        {error && error}
+      </form>
+    </div>
+  );
 }
 
 export default Login;

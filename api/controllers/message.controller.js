0de002d4bda9863d5ebdfb6fc4e48d9ae4ac1253
epinark <122348695+epinark@users.noreply.github.com>
2024-01-11 17:09:52 +0100

    add order controller routes and order fetching on frontend

---
+++ api/controllers/message.controller.js
@@ -0,0 +1,21 @@
+import createError from "../utils/createError.js";
+import Message from "../models/message.model.js";
+import Conversation from "../models/conversation.model.js";
+
+export const createMessage = async (req, res, next) => {
+    try {
+
+
+    } catch (err) {
+        next(err)
+    }
+}
+
+export const getMessages = async (req, res, next) => {
+    try {
+
+
+    } catch (err) {
+        next(err)
+    }
+}
